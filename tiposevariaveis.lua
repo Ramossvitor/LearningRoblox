7ec7866605d958c80e6eb0a46ebf8b3dd1a5ab83
Ramossvitor Windows <ramossvitor@gmail.com>
2021-09-24 00:46:29 -0300

    first commit

---
+++ tiposevariaveis.lua
@@ -0,0 +1,23 @@
+-- Declarando variaveis
+local energy = 3 
+local name = "Pedro"
+local fasepass = false
+
+-- Somando valores
+local morenergy = 4
+print(energy + morenergy)
+
+-- Concatenando texto
+print("Player Name: " .. name)
+
+-- Verificar tipos
+print(type(energy))
+
+-- Operadores relacionais
+print(energia == maisenergia)
+
+-- Nil
+nome = ""
+print(nome)
+nome = nil 
+print(nome)
